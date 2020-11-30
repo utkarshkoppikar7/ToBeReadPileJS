ba600a75cd9f7e72a6bc6ef708b650a949cc5bc3
Utkarsh Koppikar <01fe18bcs243@kletech.ac.in>
2020-11-30 15:53:15 +0530

    ver1 of goodreads feature

--- tbr.js
+++ tbr.js
@@ -1,6 +1,6 @@
 const button = document.querySelector('.submitTBR');
 const input = document.querySelector('.inputText');
-const author = document.querySelector('.author');
+const author1 = document.querySelector('.author');
 const list = document.querySelector('.list');
 
 button.addEventListener('click',addBook);
@@ -14,31 +14,64 @@ function addBook(event){
     const book = document.createElement('li');
     const deleteB = document.createElement('button');
     const checkB = document.createElement('button');
+    const link = document.createElement('button');
 
     deleteB.classList.add('TBRButton');
     checkB.classList.add('TBRCheck');
     bookDiv.classList.add('TBRDiv');
+    link.classList.add('Link');
 
-    if(input.value === '' || author.value === ''){
+    if(input.value === '' || author1.value === ''){
         alert('The field must not be empty.');
         return;
     }
 
-    const bookName = input.value +' by '+author.value; 
+    const bookName = input.value +' by '+author1.value; 
     book.innerText = bookName;
     saveBook(bookName);
     bookDiv.appendChild(book);
 
+    //?Good reads API requests
+    let url = "https://www.goodreads.com/book/title.json";
+    let param ="?author="+author1.value+"&key=uzyY7MDzUPxwxlgR05W3ZQ&title="+input.value;
+    param=param.replace(" ","+")
+    url=url+param
+
+    //! Add a proper API Page 
+    //! Right now just linking the GoodReads page
+    /*
+    const  other={
+        headers:{
+            "content-type" : "application/json; charset=UTF-8"
+        },
+        body: param,
+        method:"POST"
+    };
+    
+    fetch(url,param)
+    .then(data=>{return data.json()})
+    .then(res=>{console.log(res)})
+    .catch(error=>console.log(error))
+    */
+
+    //?Other code
     deleteB.innerHTML='<i class="fa fa-times" aria-hidden="true"></i>';
     checkB.innerHTML='<i class="fa fa-check" aria-hidden="true"></i>';
+    link.innerHTML='<i class="fa fa-link" aria-hidden="true"></i>';
+
     bookDiv.appendChild(checkB);
     bookDiv.appendChild(deleteB);
+    bookDiv.appendChild(link);
     list.appendChild(bookDiv);
 
     deleteB.addEventListener('click',remove);   
     checkB.addEventListener('click',check);
+    
     input.value='';
-    author.value='';
+    author1.value='';
+    link.addEventListener('click',function(){
+    window.open(url,'_blank');
+    });
 }
 
 function remove(event){
@@ -130,25 +163,38 @@ function divLocal(element,num){
     const book = document.createElement('li');
     const deleteB = document.createElement('button');
     const checkB = document.createElement('button');
+    const link = document.createElement('button');
 
     deleteB.classList.add('TBRButton');
     checkB.classList.add('TBRCheck');
     bookDiv.classList.add('TBRDiv');
+    link.classList.add('Link');
  
     book.innerText = element;
     bookDiv.appendChild(book);
 
     deleteB.innerHTML='<i class="fa fa-times" aria-hidden="true"></i>';
     checkB.innerHTML='<i class="fa fa-check" aria-hidden="true"></i>';
+    link.innerHTML='<i class="fa fa-link" aria-hidden="true"></i>';
+
     bookDiv.appendChild(checkB);
     bookDiv.appendChild(deleteB);
+    bookDiv.appendChild(link);
     list.appendChild(bookDiv);
 
     if(num){
         book.classList.toggle('check');       
     }
 
+    let url = "https://www.goodreads.com/book/title.json";
+    let param ="?author="+author1.value+"&key=uzyY7MDzUPxwxlgR05W3ZQ&title="+input.value;
+    param=param.replace(" ","+")
+    url=url+param
+
     deleteB.addEventListener('click',remove);   
     checkB.addEventListener('click',check);
- 
+    
+    link.addEventListener('click',function(){
+        window.open(url,'_blank');
+        });
 }
